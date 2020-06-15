6b067d6f498918220b901c8d4281d086e1564163
AmirrezaMir <amirrezamirtorabi83@gmail.com>
2020-06-15 10:00:32 +0430

    Port Scan Updated

--- modules/portscan.py
+++ modules/portscan.py
@@ -1,53 +1,50 @@
+import os
 import socket
 import sys
 from datetime import datetime
-from colorama import Fore
+from colorama import Fore 
 
 def start():
- 
-    # Clearing Terminal Window
+    # Clear Terminal Windows
     os.system("clear")
-    
-    # Asking for input 
-    ip = input(Fore.RED + "Enter a IP/Domain to scan --> ")
-    target = socket.gethostbyname(ip)
-    
-    # Scan start time
-    t1 = datetime.now()
-    
-    # Scanning ports between 1 to 1024; Using some error handling
-    try : 
-        for port in range(1, 1025):
-        	con = socket.socket(socket.AF_INET , socket.SOCK_STREAM)	# Connecting via IPV4 and TCP
-        	con.settimeout(0.5)
-        	result = con.connect_ex((target, port))
-        	if result == 0 :
-        		print(Fore.GREEN + "Port " + Fore.WHITE + str(port) + Fore.GREEN + " is open")
-        	con.close()
-    	
-    except KeyboardInterrupt :
-    	print("\nKeyboard Interruption ! !")
-    	sys.exit()
-    	
-    except socket.gaierror :
-    	print("\nCould not resolve host ! !")
-    	sys.exit()
-    	
-    except socket.error :
-    	print("\nCould Not connect to server ! !")
-    	sys.exit()
-    	
-    # Scan finish time
-    t2 = datetime.now()
-    
-    # Scan total time
-    total = t2 - t1
-    print("Scan Done In" + str(t2))
-    
-    try:
-        input(Fore.GREEN + 'Back to Menu(press enter...) ')
 
-    except:
-        print('')
+    # Asking for Input, Then get the real ip address
+    user_input = input("Enter IP/Domain to scan --> ")
+    ip = socket.gethostbyname(user_input)
+
+    # Scan Start Time
+    t1 = datetime.now()
+
+    # Scanning Ports From 1 To 1024; Also Using some Error Handling
+    try:
+        for port in range(1, 1025):
+            connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = connection.connect_ex((ip, port))
+            if result == 0:
+                print(Fore.GREEN + f"Port {port} is open")
+            connection.close()
+
+    except KeyboardInterrupt:
+        print(Fore.RED + "Keyboard Interruption ! | Exiting ...")
         sys.exit()
 
+    except socket.gaierror:
+        print(Fore.RED + "Could Not Resolve The Host ! | Exiting ...")
+        sys.exit
+
+    except socket.error:
+        print(Fore.RED + "Could Not Connect To server ! | Exiting ...")
+        sys.exit()
+
+    # Scan Finish Time
+    t2 = datetime.now()
+
+    # Estimated Time For Scan
+    total = t2 - t1
+    print(F"Scan Done in {total}")
+
+    try : 
+        input(Fore.GREEN + "Back To Menu...(Press Enter)")
+        print("")
+    except:
+        sys.exit
