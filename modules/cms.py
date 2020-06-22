4789a3e369b17fee61567beea46c69c8cf1af1c6
AmirrezaMir <amirrezamirtorabi83@gmail.com>
2020-06-22 22:22:33 +0430

    cms code updated

--- modules/cms.py
+++ modules/cms.py
@@ -11,14 +11,10 @@ def start():
         target = 'http://' + target
 
     info = builtwith.parse(target)
-    for i in info:
-        value = ''
-        for val in info[str(i)]:
-            val = val.replace('-',' ')
-            val = val.title()
-            value += str(val)
-        print(Fore.BLUE+"\n"+i+': '+value)
-
+    for keys, values in info.items():
+        keys = keys.replace('-', ' ')
+        keys = keys.title()
+        print(Fore.BLUE + '\n' + keys + ' : ' + values)
     try:
         input(Fore.GREEN + 'Back to Menu(press enter...) ')
 
